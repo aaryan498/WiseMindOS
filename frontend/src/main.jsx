d729a0deb4a292a9c4eb66209e506119dcf04f07
Geetha <25wh5a0505@bvrithyderabad.edu.in>
2026-06-30 13:00:33 +0530

    feat: add global dark/light theme toggle context (#207)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,20 @@ import App from './App.jsx'
 import './App.css'
 import { AppProvider } from './store/AppContext.jsx'
 import { FocusProvider } from './store/FocusContext.jsx'
+import { ThemeProvider } from './store/ThemeContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <BrowserRouter>
-        <AppProvider>
-          <FocusProvider>
-            <App />
-          </FocusProvider>
-        </AppProvider>
-      </BrowserRouter>
+      <ThemeProvider>
+        <BrowserRouter>
+          <AppProvider>
+            <FocusProvider>
+              <App />
+            </FocusProvider>
+          </AppProvider>
+        </BrowserRouter>
+      </ThemeProvider>
     </GoogleOAuthProvider>
 )
